08e26d4fcdd0591fc70b993d338d91a3c5bc6416
Mati <mateuszbozek077@gmail.com>
2024-11-14 09:19:11 +0100

    Feat: add rspec test for geolocation model

--- app/models/geolocation.rb
+++ app/models/geolocation.rb
@@ -4,7 +4,8 @@ class Geolocation < ApplicationRecord
   has_one :location, dependent: :delete
   has_many :languages, through: :location
 
-  validates :ip, uniqueness: true, presence: true
+  validates :ip, uniqueness: true, presence: true, format: { with: /\A(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\z/,
+                                                             message: ' has wrong format' }
 
   private
 
--- spec/models/geolocation_spec.rb
+++ spec/models/geolocation_spec.rb
@@ -4,8 +4,45 @@ RSpec.describe Geolocation, type: :model do
   
   let(:geolocation) { Geolocation.new(ip: '0.0.0.0') }
   
-  it 'new record valid' do
-    expect(geolocation).to be_valid
+  describe 'validator' do
+    context 'when attribute are valid' do
+      it 'new record is valid' do
+        expect(geolocation).to be_valid
+      end    
+    end
+
+    context 'when ip is missing' do
+      it 'new record is invalid' do
+        geolocation.ip = nil
+        expect(geolocation).not_to be_valid
+      end
+    end
+
+    context 'when ip is in wrong format' do
+      it 'new record is invalid' do
+        geolocation.ip = '266.0.0.0'
+        expect(geolocation).not_to be_valid
+      end
+    end
+
+    context 'when ip contain letter in format' do
+        it 'new record is invalid' do
+          geolocation.ip = 'a.0.0.0'
+          expect(geolocation).not_to be_valid
+        end
+      end
+
+    context 'when ip is taken' do
+        before do
+          geolocation.save
+        end
+
+        it 'new record is invalid' do
+          geolocation_2 = geolocation.dup
+          expect(geolocation_2).not_to be_valid
+        end
+      end
+
   end
 
 end
